447a42eb5c73912b54bf619c32c03171ed8df202
Doogesh Kodi Ramanah <d.ramanah@neuronsinc.com>
2024-04-17 10:08:32 +0200

    Substantially improved video montage aesthetics to match platform visualisation

--- README.md
+++ README.md
@@ -18,9 +18,10 @@ pip install -r requirements.txt
 
 Please execute the script as follows:
 
-`python gen_video_plots.py --path_video_heatmap <video-total-attention-heatmap-mp4> --path_scores_csv <video-results-frame-by-frame-csv>`
+`python gen_video_plots.py --path_video_heatmap <video-total-attention-heatmap-mp4> --path_scores_csv <video-results-frame-by-frame-csv> --metric both`
 
 The command line arguments are as follows:
 
 * --path_video_heatmap - Path to Predict video heatmap as obtained from raw downloads.
 * --path_scores_csv - Path to Predict scores (frame-level) CSV file.
+* --metric - Optional argument (default: "both") to generate only Focus graph (set "focus" here) or only Cognitive Demand graph (set "cognitive_demand" here). If not specified, both metrics will be displayed.
--- gen_video_plots.py
+++ gen_video_plots.py
@@ -1,4 +1,4 @@
-import os, cv2, json, argparse
+import os, cv2, json, argparse, glob
 import numpy as np
 import pandas as pd
 import subprocess as sp
@@ -34,7 +34,7 @@ def get_whff(file: str) -> tuple[int, int, int, int]:
     return width, height, fps, frames
 
 
-def gen_graph_video(scores_csv: str, path_video: str) -> None:
+def gen_graph_video(scores_csv: str, path_video: str, metric: str) -> None:
     '''
     Generate the video graph at the desired frame rate of 24 fps,
     ensuring alignment with the relevant video
@@ -52,78 +52,160 @@ def gen_graph_video(scores_csv: str, path_video: str) -> None:
     y_data[:,0] = scores["Cognitive Demand"]
     y_data[:,1] = scores["Focus"]
 
+    if metric == "focus":
+        focus = True
+
+    if metric == "cognitive_demand":
+        cognitive_demand = True
+
+    if metric == "both":
+        focus = True
+        cognitive_demand = True
+
     # SET UP VIDEO WRITER
-    frame_width, frame_height, fig_height = 1920, 576, 6 #1024, 576
+    frame_width, frame_height = 1280, 480 #1024, 384
     video_dir = os.path.dirname(path_video)
 
-    if frame_height == height:
-        frame_height, fig_height = int(576/1.5), int(6/1.5)
-
     # SET UP FIGURE
-    fig, ax = plt.subplots(figsize=[20, fig_height])
+    grey_color = '#808080'
+    dpi = 150
+    fig, ax = plt.subplots(figsize=(frame_width/dpi, frame_height/dpi), dpi=dpi)
+    plt.tight_layout(pad=1.0)
+
+    # CHANGE FRAME COLOUR
+    for spine in ax.spines.values():
+        spine.set_edgecolor(grey_color)
+
+    # Hide the top and right spines
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
 
     # DEFINE VIDEO PATHS
     VIDEO_GRAPH_PATH = os.path.join(video_dir, "video_graph.mp4")
     VIDEO_MONTAGE_PATH = os.path.join(video_dir, "video_montage.mp4")
     VIDEO_PADDED_PATH = os.path.join(video_dir, "video_padded.mp4")
-    out = cv2.VideoWriter(VIDEO_GRAPH_PATH, cv2.VideoWriter_fourcc(*'mp4v'), FPS, (frame_width, frame_height))
+    #out = cv2.VideoWriter(VIDEO_GRAPH_PATH, cv2.VideoWriter_fourcc(*'mp4v'), FPS, (frame_width, frame_height))
 
     # PLOT THE SCORES
     for t in range(len(x_data)):
         ax.clear()
 
-        # PLOT THE LINES
-        ax.plot(x_data[:t], y_data[:t, 0], linestyle='-', lw=2.0, color='#C5ABFD', alpha=1., label="Cognitive Demand")
-        ax.plot(x_data[:t], y_data[:t, 1], linestyle='-', lw=2.0, color='#FFD04B', alpha=1., label="Focus")
+        if focus:
+            ax.plot(x_data, y_data[:, 0], linestyle='-', lw=1.5, color='#C5ABFD', alpha=1., label="Cognitive Demand")
+        if cognitive_demand:
+            ax.plot(x_data, y_data[:, 1], linestyle='-', lw=1.5, color='#FFD04B', alpha=1., label="Focus")
 
-        # SET UP AXIS
-        ax.set_ylim(0., 100.)
+        # ADJUSTMENT TO AVOID RESIZING DUE TO ANNOTATIONS
+        y_pos_cog_demand = y_data[t, 0]
+        y_pos_focus = y_data[t, 1]
+        threshold = 95
+        if y_pos_cog_demand > threshold:
+            y_pos_cog_demand -= 10
+        if y_pos_focus > threshold:
+            y_pos_focus -= 10 
+
+        # ADD DYNAMIC MARKERS
+        if cognitive_demand:
+            ax.scatter(x_data[t], y_data[t, 0], color='#C5ABFD', marker="o", s=20)
+            offset_x = 0.005 * (max(x_data) - min(x_data))
+            label_x_position = x_data[t] + offset_x if t <= num_frames_video / 2 else x_data[t] - offset_x
+            offset_y = 0.005 * (max(y_data[:,0]) - min(y_data[:,0]))
+            label_y_position = y_pos_cog_demand + offset_y
+            
+            ax.text(label_x_position, label_y_position, f"Cognitive Demand: {y_data[t, 0]:.0f}", fontsize=8, verticalalignment='bottom', 
+                    ha='right' if t > num_frames_video / 2 else 'left', color=grey_color, 
+                    bbox=dict(facecolor='white', alpha=0.7, edgecolor='none', boxstyle='round,pad=0.1'))
+
+        if focus:
+            ax.scatter(x_data[t], y_data[t, 1], color='#FFD04B', marker="D", s=20)
+            offset_x = 0.005 * (max(x_data) - min(x_data))
+            label_x_position = x_data[t] + offset_x if t <= num_frames_video / 2 else x_data[t] - offset_x            
+            offset_y = 0.005 * (max(y_data[:,1]) - min(y_data[:,1]))
+            label_y_position = y_pos_focus + offset_y
+            
+            ax.text(label_x_position, label_y_position, f"Focus: {y_data[t, 1]:.0f}", 
+                    fontsize=8, verticalalignment='bottom', 
+                    ha='right' if t > num_frames_video / 2 else 'left', color=grey_color, 
+                    bbox=dict(facecolor='white', alpha=0.7, edgecolor='none', boxstyle='round,pad=0.1'))
+
+        # SET UP AXES
+        ax.set_ylim(0., 105.)
         ax.set_xlim(0, num_frames_video/FPS)
         ax.set_xticks(np.arange(0, num_frames_video/FPS, 2))
         ax.set_xticklabels([f"{int(t//60):02d}:{int(t%60):02d}" for t in np.arange(0, num_frames_video/FPS, 2)])
+        ax.tick_params(axis='x', colors="darkgrey")
+        ax.tick_params(axis='y', colors="darkgrey")
+        plt.xticks(fontsize=6)
+        plt.yticks(fontsize=6)
 
-        # ADD GRID
-        ax.grid(color='lightgrey', linestyle='-', alpha=0.35)
+        # ADD HORIZONTAL GRID ONLY
+        ax.yaxis.grid(color='lightgrey', linestyle='-', alpha=0.35)
 
         # INSERT LEGEND
-        legend = ax.legend(loc='upper left', fontsize=12, ncol=2)
-        legend.get_frame().set_alpha(0.7)
-        legend.get_frame().set_facecolor('white')
+        plt.subplots_adjust(bottom=0.0001)
+        legend = ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1), fontsize=8, ncol=2, framealpha=0.7,
+                           facecolor='white', frameon=True)
         legend.get_frame().set_linewidth(0)
+        plt.tight_layout(rect=[0, 0.0001, 1, 1])
+        '''
+        # INSERT LEGEND
+        legend = ax.legend(loc='upper center', fontsize=8, ncol=2)
+        legend.get_frame().set_alpha(0.)
+        #legend.get_frame().set_facecolor('white')
+        legend.get_frame().set_linewidth(0)
+        '''
+        for text in legend.get_texts():
+            text.set_color(grey_color)
 
         # SAVE CURRENT FIGURE AS AN IMAGE
-        temp_img_path = os.path.join(video_dir, 'temp_plot.png')
-        plt.savefig(temp_img_path, bbox_inches='tight')
-
-        # READ THE IMAGE AND RESIDE TO DESIRED FRAME SIZE
-        frame = cv2.imread(temp_img_path)
-        frame = cv2.resize(frame, (frame_width, frame_height))
-
-        # WRITE FRAME TO VIDEO
-        out.write(frame)
+        temp_img_path = os.path.join(video_dir, f'temp_plot_{t:04d}.png')
+        plt.savefig(temp_img_path, dpi=dpi) #bbox_inches='tight'
 
     # RELEASE VIDEO WRITER AND CLOSE FIGURE
-    out.release()
     plt.close()
 
     # CALCULATE PADDING DIMENSIONS
     pad_width = max(frame_width, width)
     pad_height = max(frame_height, height)
 
+    # GENERATE VIDEO GRAPH
+    sp.call([
+        'ffmpeg',
+        '-r', str(FPS),
+        '-f', 'image2',
+        '-s', f'{frame_width}x{frame_height}',
+        '-y',
+        '-i', os.path.join(video_dir, 'temp_plot_%04d.png'),
+        '-vcodec', 'libx264',
+        '-crf', '18',
+        '-pix_fmt', 'yuv420p', VIDEO_GRAPH_PATH,
+        '-loglevel', 'panic'
+    ])
+
     # PAD THE VIDEO
     sp.call([
-        'ffmpeg', '-y', '-i', path_video, '-vf', 'pad=width={}:height={}:x=(ow-iw)/2:y=(oh-ih)/2:color=white'.format(pad_width, pad_height),
-        VIDEO_PADDED_PATH, '-loglevel', 'panic', 
+        'ffmpeg',
+        '-y',
+        '-i', path_video,
+        '-vf', 'pad=width={}:height={}:x=(ow-iw)/2:y=(oh-ih)/2:color=white'.format(pad_width, pad_height), VIDEO_PADDED_PATH,
+        '-loglevel', 'panic', 
     ])
 
     # STACK THE PADDED VIDEO AND GRAPH
     sp.call([
-        'ffmpeg', '-y', '-i', VIDEO_PADDED_PATH, '-i', VIDEO_GRAPH_PATH,
-        '-filter_complex', 'vstack', VIDEO_MONTAGE_PATH, '-loglevel', 'panic'
+        'ffmpeg',
+        '-y',
+        '-i', VIDEO_PADDED_PATH,
+        '-i', VIDEO_GRAPH_PATH,
+        '-filter_complex', 'vstack', VIDEO_MONTAGE_PATH,
+        '-loglevel', 'panic'
     ])
 
     # DELETE TEMP FILES
-    os.remove(temp_img_path)
+    frame_files = glob.glob(os.path.join(video_dir, 'temp_plot_*.png'))
+    for file in frame_files:
+        os.remove(file)
+
     os.remove(VIDEO_PADDED_PATH)
     os.remove(VIDEO_GRAPH_PATH)
 
@@ -132,6 +214,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_video_heatmap', type=str, help='Path to video heatmap')
     parser.add_argument('--path_scores_csv', type=str, help='Path to CSV file containing frame-level scores')
+    parser.add_argument('--metric', type=str, help='Cognitive Demand or Focus or both', default='both')
 
     opt = parser.parse_args()
-    gen_graph_video(opt.path_scores_csv, opt.path_video_heatmap)
+    gen_graph_video(opt.path_scores_csv, opt.path_video_heatmap, opt.metric)
